98cac6d38ad18a17e4475bf845a7b644eae06aaf
Nicholas Williams <nicholaswilliams910@gmail.com>
2024-05-30 15:10:50 -0500

    implemented click event to display the number of orders that each employee was responsible for selling

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,44 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener("click", (clickEvent) => {
+	const itemClicked = clickEvent.target
+
+	// Was an employee list item clicked?
+	if (itemClicked.dataset.type === "employee") {
+		// Get the id of the employee clicked
+		const employee = parseInt(itemClicked.dataset.id)
+
+		// start a counter variable at 0
+		let orderCounter = 0
+
+		// Iterate through all the orders
+		for (const order of orders) {
+			// Does the employeeId foreign key match the id of the employee clicked?
+			if (employee === order.employeeId) {
+				// increase the counter by 1
+				orderCounter++
+			}
+		}
+
+		const employeeName = itemClicked.dataset.employeename
+
+		// display window alert
+		window.alert(`${employeeName} sold ${orderCounter} product(s)`)
+	}
+})
 
 export const Employees = () => {
 	let html = "<ul>"
 
 	for (const employee of employees) {
-		html += `<li>${employee.name}</li>`
+		html += `<li
+                data-type="employee"
+                data-id="${employee.id}"
+                data-employeename="${employee.name}"
+                >${employee.name}</li>`
 	}
 
 	html += "</ul>"
